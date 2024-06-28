fc83fc96b0ec296a4c864320b5caf12c2fa58525
LinThitHtwe <linthit2745@gmail.com>
2024-06-28 10:52:28 +0630

    [init] models and [add] employee data access layer

---
+++ Batch4.Api.PayrollManagementSystem.DataAccess/Db/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Batch4.Api.PayrollManagementSystem.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch4.Api.PayrollManagementSystem.DataAccess.Db
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Batch4.Api.PayrollManagementSystem.DataAccess/Db/ConnectionString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch4.Api.PayrollManagementSystem.DataAccess.Db
+{
+    internal class ConnectionString
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new()
+        {
+            DataSource = "",
+            InitialCatalog = "DotnetTrainingBatch4",
+            UserID = "",
+            Password = "",
+            TrustServerCertificate = true,
+        };
+    }
+}
