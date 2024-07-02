1795796c3f6788710a6bd751405233ad5ad8a663
LinThitHtwe <linthit2745@gmail.com>
2024-07-02 22:20:43 +0630

    [refactor] services and controllers

---
+++ Batch4.Api.PayrollManagementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using Batch4.Api.PayrollManagementSystem.BusinessLogic.Services;
+using Batch4.Api.PayrollManagementSystem.DataAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Batch4.Api.PayrollManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeService _employeeService;
+
+        public EmployeeController()
+        {
+            _employeeService = new EmployeeService();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _employeeService.GetEmployees();
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var employee = await _employeeService.GetbyEmployeeId(id);
+            if(employee is null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            var result = await _employeeService.CreateEmployee(employee);
+            string message = result > 0 ? "Create Successful" : "Create Failed";
+            return Ok(message);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Employee employee)
+        {
+            var result = await _employeeService.UpdateEmployee(id, employee);
+            string message = result > 0 ? "Update Successful" : "Update Failed";
+            return Ok(message);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _employeeService.DeleteEmployee(id);
+            string message = result > 0 ? "Delete Successful" : "Delete Failed";
+            return Ok(message);
+        }
+    }
+}
--- Batch4.Api.PayrollManagementSystem/Controllers/PayrollController.cs
+++ Batch4.Api.PayrollManagementSystem/Controllers/PayrollController.cs
@@ -16,36 +16,5 @@ namespace Batch4.Api.PayrollManagementSystem.Controllers
             _payrollService = new PayrollService();
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Read()
-        {
-            var list =await _payrollService.GetEmployees();
-            return Ok(list);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(Employee employee)
-        {
-            var result =await _payrollService.CreateEmployee(employee);
-            string message = result > 0 ? "Create Successful" : "Create Failed";
-            return Ok(message);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id,  Employee employee)
-        {
-            var result = await _payrollService.UpdateEmployee(id, employee);
-            string message = result > 0 ? "Update Successful" : "Update Failed";
-            return Ok(message);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAsync (int id)
-        {
-            var result = await _payrollService.DeleteEmployee(id);
-            string message = result > 0 ? "Delete Successful" : "Delete Failed";
-            return Ok(message);
-        }
-
     }
 }
